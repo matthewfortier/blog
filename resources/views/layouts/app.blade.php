f1fd8e54c354622cd0727fa380b134516451279d
Matthew Fortier <matther2012@gmail.com>
2017-07-13 13:52:02 -0400

    Google Site Verification

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
+    <meta name="google-site-verification" content="OzuhB6KSXLO53pEUSavapnt2VpjaJqjhJp8SdVJlBgk" />
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
