de022f85e52f51d3b71a11e71920f9559e4cde91
Matthew Fortier <matthewfortier@Matthews-MacBook-Pro.local>
2017-07-15 01:03:13 -0400

    Made the header a laravel partial and some minor styling updates

---
+++ resources/views/partials/_header.blade.php
@@ -0,0 +1,45 @@
+<nav>
+    <div class="logo">
+        <div class="spinner">
+            <span></span>   
+        </div>
+        <div class="text">
+            <span style="color: white; margin-right: -15px;">MATTHEW</span>
+            <span style="color: rgba(255,255,255,.7);">FORTIER</span>
+        </div>
+    </div>
+    <div class="links">
+        <ul>
+            <a href="/blog">
+                <li class="link">
+                    <span></span>
+                    <span>Blog</span>
+                </li>
+            </a>
+            <a href="/blog">
+                <li class="link">
+                    <span></span>
+                    <span>Portfolio</span>
+                </li>
+            </a>
+            <a href="/blog">
+                <li class="link">
+                    <span></span>
+                    <span>Resume</span>
+                </li>
+            </a>
+            <a href="/about">
+                <li class="link">
+                    <span></span>
+                    <span>About Me</span>
+                </li>
+            </a>
+            <a href="/contact">
+                <li class="link">
+                    <span></span>
+                    <span>Contact</span>
+                </li>
+            </a>
+        </ul>
+    </div>
+</nav>
