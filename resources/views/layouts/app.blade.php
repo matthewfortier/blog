395cd2d4cde97312c8d42c91b6f99909dfa3cbb2
Matthew Fortier <matthewfortier@Matthews-MacBook-Pro.local>
2017-07-15 01:01:49 -0400

    Made the header a laravel partial and some minor styling updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,42 +11,9 @@
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
   </head>
   <body>
-    <nav>
-       <div class="logo">
-	   		<div class="spinner">
-				<span></span>   
-			</div>
-			<div class="text">
-				<span style="color: white; margin-right: -15px;">MATTHEW</span>
-				<span style="color: rgba(255,255,255,.7);">FORTIER</span>
-			</div>
-	   </div>
-	   <div class="links">
-	   		<ul>
-				<li class="link">
-					<span></span>
-					<span>Blog</span>
-				</li>
-				<li class="link">
-					<span></span>
-					<span>Portfolio</span>
-				</li>
-				<li class="link">
-					<span></span>
-					<span>Resume</span>
-				</li>
-				<li class="link">
-					<span></span>
-					<span>About Me</span>
-				</li>
-				<li class="link">
-					<span></span>
-					<span>Contact</span>
-				</li>
-			</ul>
-	   </div>
-    </nav>
 
+  	@include("partials._header")
+    
     <div class="container">
         @yield('content')
     </div>
