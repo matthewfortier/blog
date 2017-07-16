601fd0bdce248ff8818a008094228e6bbf783f47
Matthew Fortier <matthewfortier@Matthews-MacBook-Pro.local>
2017-07-16 18:54:24 -0400

    Updated nav bar, making mobile friendly

--- resources/views/partials/_header.blade.php
+++ resources/views/partials/_header.blade.php
@@ -4,7 +4,7 @@
             <span></span>   
         </div>
         <div class="text">
-            <span style="color: white; margin-right: -15px;">MATTHEW</span>
+            <span style="color: white;">MATTHEW</span>
             <span style="color: rgba(255,255,255,.7);">FORTIER</span>
         </div>
     </div>
@@ -42,4 +42,30 @@
             </a>
         </ul>
     </div>
+    <div class="menu-icon">
+        <span></span>
+        <div class="menu-icon-middle">
+            <span></span>
+            <span></span>    
+        </div>
+        <span></span>
+    </div>
 </nav>
+<div class="mobile-links">
+    <ul>
+        <li>Blog</li>
+        <li>Portfolio</li>
+        <li>Resume</li>
+        <li>About Me</li>
+        <li>Contact</li>
+    </ul>
+</div>
+
+@section('scripts')
+    <script>
+        $(".menu-icon").click(function(){
+            $(this).toggleClass("active");
+            $('.mobile-links').toggleClass("active");
+        });
+    </script>
+@endsection
